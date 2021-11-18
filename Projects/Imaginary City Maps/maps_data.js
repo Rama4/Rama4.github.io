5e876fd7b03a5b753c0dcd09efd202a08c907a9c
Rama Narasimhan <ranras@amazon.com>
2021-11-18 11:20:05 +0530

    group undergrad projects

---
+++ Projects/Imaginary City Maps/maps_data.js
@@ -0,0 +1,260 @@
+
+const m5_areas = [
+    {
+        "left": "0.5064715813168261%",
+        "right": "54.33314575126618%",
+        "top": "38.58955538436941%",
+        "bottom": "2.108980974113763%"
+    },
+    {
+        "left": "24.330357142857142%",
+        "right": "34.22619047619048%",
+        "top": "3.3142309214527277%",
+        "bottom": "27.601751715838475%"
+    },
+    {
+        "left": "96.74479166666667%",
+        "right": "0.0186011904761898%",
+        "top": "22.137775475334724%",
+        "bottom": "30.78727464034158%"
+    },
+    {
+        "left": "36.27232142857143%",
+        "right": "57.124255952380956%",
+        "top": "87.3927299287923%",
+        "bottom": "0.7983113510800166%"
+    },
+    {
+        "left": "51.74451322453574%",
+        "right": "40.02532357906584%",
+        "top": "3.7305659316967286%",
+        "bottom": "80.16017209052194%"
+    },
+    {
+        "left": "40.19414743950478%",
+        "right": "47.172200337647716%",
+        "top": "54.044757101398716%",
+        "bottom": "29.894429829023807%"
+    },
+    {
+        "left": "36.986494091164886%",
+        "right": "53.038829487900955%",
+        "top": "18.313687301056667%",
+        "bottom": "52.907661225854284%"
+    },
+    {
+        "left": "84.1024198086663%",
+        "right": "10.453010692177827%",
+        "top": "64.00100773729064%",
+        "bottom": "25.89739490220589%"
+    },
+    {
+        "left": "55.96510973550929%",
+        "right": "33.37084974676421%",
+        "top": "40.01879817638309%",
+        "bottom": "31.880835065381817%"
+    },
+    {
+        "left": "0.16882386043894204%",
+        "right": "90.30669667979741%",
+        "top": "23.861087290397908%",
+        "bottom": "62.04028042228069%"
+    },
+    {
+        "left": "74.6060776589758%",
+        "right": "18.14856499718627%",
+        "top": "18.822400837197133%",
+        "bottom": "63.05770749456161%"
+    },
+];
+
+const m5_area_descriptions = [
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This city is located on a group of small islands located at the confluence of two rivers and cris-crossed by canals. The main influence for this city was Stockholm and Amsterdam.</p>",
+    },
+    {
+            left: "100px",
+            top: "800px",
+            description:  "<p>This area was inspired from Manhattan island. It is the center of this city, and contains the main train station at the top-left corner of the island.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This area contains the Airport of the city. Its only partially visible since it's at the outskirts of the city.</p>", 
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This area contains an University</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This area contains the major train station of the city which serves 2 lines (passenger cum freight), an above-ground (vertical) line and a below-ground (horizontal) line.</p><p>The station is adjacent to a port on the river. This is so that goods can be moved back and forth from the port to inland areas via rail.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This is a riverfront park at the end of the island. It was inspired by the battery park in Manhattan.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This was very first part of the city that i drew. It consists of two elevated expressways on top of another (one from north-south and another from east-west), and a ground-level road. I couldn't draw the ramps conecting one expressway to another, but we can assume that they are present in the area where the two expressways overlap on each other.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This is a park that is aptly named dinosaur's foot, due to a friend's suggestion that it looks exactly like one</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This big empty space contains the main port of the city.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>A park containing a highway interchange. This is inspired from the intechanges in the US, which are vast and contain parks under them.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This is a stadium district, which contains many stadiums for different sports, especially soccer. It's close proximity to a canal is inspired from the Millenium Dome of London, which is adjacent to the Thames river.</p>",
+    },
+];
+
+const m4_areas = [
+    {
+        "left": "29.938097917839055%",
+        "right": "33.7647720877884%",
+        "top": "16.865358223515603%",
+        "bottom": "29.23163412093453%"
+    },
+    {
+        "left": "75.35171637591446%",
+        "right": "2.5886325267304358%",
+        "top": "50.67874799517188%",
+        "bottom": "21.543014930802414%"
+    },
+    {
+        "left": "56.949915588069786%",
+        "right": "32.751828925154754%",
+        "top": "19.345557962267897%",
+        "bottom": "69.0801765902214%"
+    },
+    {
+        "left": "82.49859313449635%",
+        "right": "8.047270680922907%",
+        "top": "20.25496453314374%",
+        "bottom": "67.59205674697002%"
+    },
+    {
+        "left": "0.5627462014631401%",
+        "right": "86.43781654473833%",
+        "top": "73.0832189685676%",
+        "bottom": "5.6697366027877365%"
+    },
+    {
+        "left": "77.94034890264491%",
+        "right": "11.198649409116484%",
+        "top": "84.57481109145324%",
+        "bottom": "0.5439904760329881%"
+    },
+    {
+        "left": "19.977490151941474%",
+        "right": "58.300506471581315%",
+        "top": "65.39459977843549%",
+        "bottom": "16.16924883017245%"
+    },
+];
+const m4_area_descriptions = [
+    {
+        left: "100px",
+        top: "800px",
+        description:  "<p>The center of this city contains a huge public garden, surrounded by an inner circular route of expressway, from which other expressways branch out in various directions.</p><p>This city was heavily infuenced by the city map of <em>Tokyo</em>, which also contains a huge park in the center containing the Japanese imperial palace.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description:  "<p>This is another huge park in the city. This was inspired from New York's Central Park.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description:  "<p>This is a point where a radial highway ends into the inner circular highway. This was influenced by Yamuna expressway in India.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>In this city, metro stations are depicted with shaded blocks. In this case, a road passes over the metro station. Also, above-ground train lines are depicted by continuous lines, whereas underground train lines are depicted by dotted lines.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This heart-shaped interchange involves two expressways crossing over and a third one converging at the same point. So I had to design a five-way interchange and came up with this design. It took a lot of time and effort to come up with the design and draw it.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>This interchange is taken from Shanghai's Puxi Viaduct, which is one of the largest and busiest interchanges in the world. I was impressed by its design and wanted to incorporate it into my imaginary city.</p>",
+    },
+    {
+        left: "100px",
+        top: "800px",
+        description: "<p>The main idea that is being used in this city, is the concept of skip roads, which skip an entire neighbourhood and take traffic to the next neighbourhood without any intersections in between. This city makes ample use of such kind of roads.</p><p>In this picture, a few skip roads are present. The most easily recognizable one is the one that begins from the upper left corner and curves towards the right side. Another skip road is present at the center, going north-south.</p>",
+    },
+];
+const m3_areas = [
+    {
+        "left": "35.340461451885204%",
+        "right": "45.694991558806976%",
+        "top": "30.067514523473072%",
+        "bottom": "46.45941278065631%"
+    }
+];
+const m3_area_descriptions = [
+    {
+        left: 131,
+        top: 22,
+        description: '<p>This is the  central roundabout of the city.vfaaf w</p>'
+    }
+];
+const m2_areas = [];
+const m2_area_descriptions = [];
+const m1_areas = [];
+const m1_area_descriptions = [];
+
+
+const cities = [
+    {
+        areas: m5_areas,
+        descriptions: m5_area_descriptions,
+        img_uri: "m5/image-001.jpg",
+    },
+    {
+        areas: m4_areas,
+        descriptions: m4_area_descriptions,
+        img_uri: "m4/image-001.jpg",
+    },
+    {
+        areas: m3_areas,
+        descriptions: m3_area_descriptions,
+        img_uri: "m3/image-001.jpg",
+    },
+    {
+        areas: m2_areas,
+        descriptions: m2_area_descriptions,
+        img_uri: "m2/image-001.jpg",
+    },
+    {
+        areas: m1_areas,
+        descriptions: m1_area_descriptions,
+        img_uri: "m1/image-001.jpg",
+    },
+    
+];
+
+
